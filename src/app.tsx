d543d33bd597f7ce367ee4b77d2a5c34ccb96a60
ecto0310 <ecto0310@gmail.com>
2020-11-04 00:45:49 +0900

    Add router

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './component/home';
+import Play from './component/play';
+import Json from './component/json';
 
 function App() {
   return (
@@ -15,6 +20,13 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/:id' component={Play} />
+          <Route exact path='/:id/json' component={Json} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/component/play.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Play extends React.Component {
+  render() {
+    return (
+      <div className="home">
+      play
+      </div>
+    );
+  }
+}
+
+export default Play;
