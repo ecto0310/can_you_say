514ab8f0af3c2442ba90bf713d74aece4cbc2714
ecto0310 <ecto0310@gmail.com>
2020-11-03 23:47:47 +0900

    Add navigation bar

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="app">
+      <Navbar bg="dark" variant="dark" className="mr-auto">
+        <Navbar.Brand href={process.env.PUBLIC_URL}>言えるかな?</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Nav.Link href="https://github.com/ecto0310/can_you_say">ソースコード</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
